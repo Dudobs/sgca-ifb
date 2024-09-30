9df5eb8599f44ef54481c8906d963e0aeb938501
Eduardo Vieira <3413850@ifb.local>
2024-09-30 12:33:17 -0300

    Desenvolvendo componente da navbar

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -9,7 +9,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
       {...props}
       ref={ref}
       className={twMerge(
-        'px-4 w-80 h-14 bg-zinc-50 border-2 rounded-lg outline-none text-md focus-visible:border-emerald-500 focus-visible:ring-4 ring-zinc-500/10',
+        'px-4 w-80 h-14 bg-zinc-50 border-2 rounded-lg outline-none text-md focus-visible:border-emerald-500 focus-visible:ring-2 ring-zinc-500/10',
         props.className
       )}
     />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,20 @@
+import LogoIFBAccess from '../assets/images/Logo-ifb-access.svg'
+import CloseNavbar from '../assets/images/close-navbar.svg'
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <div className="h-screen w-80 fixed top-0 left-0 py-8 px-5 bg-green-600">
+      <button
+        className="size-5 absolute top-8 right-2.5 hover:bg-green-700"
+        type="button"
+      >
+        <img src={CloseNavbar} alt="Fechar barra de navegação" />
+      </button>
+      <div className="flex items-end gap-2">
+        <img className="size-16" src={LogoIFBAccess} alt="" />
+        <span className="text-zinc-50 font-bold text-lg uppercase">
+          Sistema de Gerenciamento
+        </span>
+      </div>
+    </div>
+  )
 }
